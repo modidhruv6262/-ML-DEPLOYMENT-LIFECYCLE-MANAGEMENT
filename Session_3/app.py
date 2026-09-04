5e05d201b20180477c7acc7091fafb69a0563031
Dhruv Modi <modidhruv6262@gmail.com>
2026-09-04 15:58:11 +0530

    push

--- Session_3/app.py
+++ Session_3/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
-
 app = Flask(__name__)
 delivery_model = joblib.load('delivery_model.joblib')
 
@@ -14,7 +13,7 @@ def predict_price():
     data = request.get_json()
     base_price = data.get('base_price', 0)
     discount = data.get('discount', 0)
-    final_price = base_price - (base_price * discount / 100)
+    final_price = base_price - base_price * discount / 100
     return jsonify({'final_price': final_price})
 
 @app.route('/predict-delivery', methods=['POST'])
@@ -22,15 +21,9 @@ def predict_delivery():
     data = request.get_json()
     distance = data.get('distance', 0)
     order_size = data.get('order_size', 0)
-    
     features = np.array([[distance, order_size]])
     prediction = delivery_model.predict(features)[0]
     estimated_time = round(prediction, 1)
-    
-    return jsonify({
-        'estimated_time_minutes': estimated_time,
-        'message': f"Hang tight! Your delicious food will be at your door in approximately {estimated_time} minutes."
-    })
-
+    return jsonify({'estimated_time_minutes': estimated_time, 'message': f'Hang tight! Your delicious food will be at your door in approximately {estimated_time} minutes.'})
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ Session_5/IPL_Team_App/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title('My Favorite IPL Team: Chennai Super Kings')
+st.image('logo.png', width=300)
+st.write('CSK is one of the most successful and consistent teams in IPL history, known for their brilliant leadership and strategic gameplay.')
